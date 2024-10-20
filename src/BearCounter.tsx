b1367df5f442e17d3f58d5e295adff329ab2b053
Jenya <113167825+Genya10@users.noreply.github.com>
2024-10-20 21:49:16 +0300

    create persist for localStorage and useUserStore

--- src/BearCounter.tsx
+++ src/BearCounter.tsx
@@ -1,15 +1,30 @@
-import { useBearStore } from "./store/store"
+import { useBearStore, useUserStore } from "./store/store"
 
 export function BearCounter(){
     const bears = useBearStore(state => state.bears)
     const increasePopulation = useBearStore(state => state.increasePopulation)
     const removeAllBears = useBearStore(state => state.removeAllBears)
 
+    const users = useUserStore(state => state.users)
+    const fetchUsers = useUserStore(state => state.fetchUsers)
+
     return (
         <div>
             <h1>{bears}</h1>
             <button onClick={increasePopulation}>Increase bears</button>
             <button onClick={removeAllBears}>Remove bears</button>
+            <div>
+                <h2>Users List</h2>
+                {users.length > 0 ? (
+                    <ul>
+                     {users.map((user)=> (
+                        <li key={user.id}>{user.name}</li>
+                     ))}
+                    </ul>
+                ):(
+                    <p>No users</p>
+                )}
+            </div>
         </div>
     )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import {create} from 'zustand'
+import {persist} from 'zustand/middleware'
 
 interface IBearStore {
     bears: number
@@ -6,8 +7,39 @@ interface IBearStore {
     removeAllBears:()=> void
 }
 
-export const useBearStore = create<IBearStore>(set=>({
+interface IUser {
+    id:number
+    name: string
+    email: string
+}
+interface IUserStore {
+    users:IUser[]
+    fetchUsers:() => Promise<void>
+}
+
+/*export const useBearStore = create<IBearStore>(set=>({
   bears:0,
   increasePopulation:()=> set(state => ({bears: state.bears + 1})),
   removeAllBears:()=> set({bears:0})
+}))*/
+
+export const useBearStore = create<IBearStore>()(
+persist(
+  set=>({
+    bears:0,
+    increasePopulation:()=> set(state => ({bears: state.bears + 1})),
+    removeAllBears:()=> set({bears:0}),
+  }),
+{
+    name:'bear-storage',
+})
+)
+
+export const useUserStore = create<IUserStore>(set => ({
+    users:[],
+    fetchUsers: async()=> {
+        const res = await fetch('https://jsonplaceholder.typecode.com/users')
+        const users = await res.json()
+        set({users})
+    }
 }))
